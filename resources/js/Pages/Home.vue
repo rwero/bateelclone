26b86e2a653fe8a1b9d9e5612e9b37ec43914208
rwero <redgit115@protonmail.com>
2022-10-05 10:27:14 +0100

    update for mobile

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -14,31 +14,30 @@ defineProps({
 
 	<Head title="Welcome" />
 	<AuthenticatedLayout>
-		<template #header>
-			<div>
-				Main page
 
-			</div>
-		</template>
 
 		<template #main>
-			<div class="md:h-[390px]  mx-auto   overflow-hidden relative">
-				<img src="https://bateel.com/media/wysiwyg/HomePageBanners/2022/palm/international/wide.jpg"
-					class="object-contain  w-full object-center " alt="" srcset="">
-				<div
-					class="absolute  inset-0 w-96 mx-auto text-center  text-white flex items-center justify-center flex-col gap-6 ">
-					<h2 class="font-bold text-2xl text-shadow ">Discover the Difference</h2>
-					<span class="text-md text-shadow ">Cultivators of the world’s only gourmet dates packaged in luxury
-						hand-painted wooden gift boxes</span>
-					<a :href="route('products.index')" class="py-2 px-10 bg-white rounded-lg text-black">
-						<span>Shop now</span>
-					</a>
+			<div class=" mx-auto mb-20 w-[100vw] absolute  overflow-hidden inset-x-0">
+				<div class=" md:h-[590px] sm:h-[360px] h-[250px] relative">
+
+					<img src="https://bateel.com/media/wysiwyg/HomePageBanners/2022/palm/international/wide.jpg"
+						class="object-cover  h-full object-center w-full" alt="" srcset="">
+					<div
+						class="absolute  inset-0 md:w-4/5  w-[90%] mx-auto text-center  text-white flex items-center justify-center flex-col gap-6 ">
+						<h2 class="font-bold md:text-4xl text-2xl text-shadow ">Discover the Difference</h2>
+						<span class="md:text-xl  text-md text-shadow  ">Cultivators of the world’s only gourmet dates packaged in
+							luxury
+							hand-painted wooden gift boxes</span>
+						<a :href="route('products.index')" class="py-3 px-10 mt-2 uppercase bg-white rounded-lg text-black">
+							<span>Shop now</span>
+						</a>
+					</div>
 				</div>
 
 
 			</div>
 
-			<div class="md:h-[590px]  mx-auto  flex justify-center items-center">
+			<div class=" mx-auto mb-20 md:mt-[680px] sm:mt-[450px] mt-[340px] flex justify-center items-center">
 				<div class="grid md:grid-cols-2 grid-cols-1 gap-6">
 					<div>
 						<img src="https://bateel.com/media/wysiwyg/HomePageBanners/2022/palm/international/main.jpg"
@@ -67,7 +66,7 @@ defineProps({
 
 			</div>
 
-			<div class="md:h-[590px]  mx-auto">
+			<div class="my-20 mx-auto">
 				<h2 class="font-semibold text-3xl mt-4 mb-8">Premium Organic Dates</h2>
 				<div class="grid md:grid-cols-3 gap-6 items-center grid-cols-1">
 					<div>
@@ -111,22 +110,25 @@ defineProps({
 
 			</div>
 
-			<div class="h-[590px]  mx-auto ">
+			<div class="  my-20 mx-auto ">
 
 				<div class="flex items-center justify-center ">
-					<div class=" mr-[-5rem]"><img src="https://bateel.com/media/wysiwyg/boutiquePage/Farm-image-for-web.jpg"
-							class="object-contain object-center mx-auto  relative img-shadow
+					<div class=" mr-[-5rem]"><img
+							src="https://bateel.com/media/wysiwyg/boutiquePage/Farm-image-for-web.jpg" class="object-contain object-center mx-auto  relative img-shadow
 							" alt="" srcset="" />
 					</div>
 					<div class="bg-white shadow pt-14  border border-gray-100 px-10 w-[500px] z-10">
-<h2 class="font-medium text-2xl">Bateel Organic Date Groves</h2>
-<p class="text-gray-600 mt-4 text-sm font-sans">Bateel date groves have a rich heritage dating back nearly 100 years when the first date palms were planted in 1932. </p>
-<div class="text-center mt-6 mb-8"><button class="bg-white rounded-md text-sm mx-auto px-6 py-2  border-gray-800 border ">Discover</button></div>
+						<h2 class="font-medium text-2xl">Bateel Organic Date Groves</h2>
+						<p class="text-gray-600 mt-4 text-sm font-sans">Bateel date groves have a rich heritage dating
+							back nearly 100 years when the first date palms were planted in 1932. </p>
+						<div class="text-center mt-6 mb-8"><button
+								class="bg-white rounded-md text-sm mx-auto px-6 py-2  border-gray-800 border ">Discover</button>
+						</div>
 					</div>
 				</div>
 
 			</div>
-<div class="md:h-[590px]  mx-auto">
+			<div class="md:h-[590px]  mx-auto">
 				<h2 class="font-semibold text-3xl mt-4 mb-8">Gourmet Products</h2>
 				<div class="grid md:grid-cols-3 gap-6 items-center grid-cols-1">
 					<div>
@@ -170,7 +172,7 @@ defineProps({
 
 			</div>
 
-	
+
 		</template>
 
 	</AuthenticatedLayout>
@@ -182,7 +184,8 @@ defineProps({
 .text-shadow {
 	text-shadow: 2px 2px 2px hsl(0, 0%, 17%);
 }
-.img-shadow{
+
+.img-shadow {
 	box-shadow: 15px 15px 0px #f0e2e1;
 }
 </style>
